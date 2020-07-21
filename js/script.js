16ce4dbb6ac753d1a1bc9baf27e8c63a5bf14942
caterina <catemarian1806@gmail.com>
2020-07-21 17:02:23 +0200

    click, numero e colore ma si possono ancora aggiungere numeri

---
+++ js/script.js
@@ -0,0 +1,55 @@
+function confrontoCinque(selected,value) {
+  var confronto = value<=5;
+  if (confronto) {
+    selected.addClass("min");
+  }
+  else {
+    selected.addClass("max");
+  }
+}
+
+
+function getRandomNum(selected) {
+  $.ajax({
+    url:"https://flynn.boolean.careers/exercises/api/random/int",
+    method:"GET",
+    success:function (data,state) {
+      // selected.children("span") = "";
+      var success = data["success"];
+      var value = data["response"];
+
+      if (success) {
+        console.log(data);
+        selected.children("span").append(value);
+        confrontoCinque(selected,value);
+      }
+      else {
+        console.log("error");
+      }
+    },
+    error:function(request,state,error){
+      console.log("request",request);
+      console.log("state",state);
+      console.log("error",error);
+    }
+  })
+}
+
+function selectCell(){
+  var target = $("#tabella td");
+  target.click(
+    function () {
+      var selected = $(this);
+      getRandomNum(selected)
+      // console.log(selected);
+    }
+
+  );
+}
+
+function init() {
+  selectCell();
+}
+
+
+$(document).ready(init)
